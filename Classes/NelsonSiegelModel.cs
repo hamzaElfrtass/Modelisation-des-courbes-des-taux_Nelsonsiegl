10ff2252a8bc7dcfef0628f1a2c3dcd2059029ec
hamzaelfrtass <hamzaelfrtass5@gmail.com>
2023-11-16 13:12:47 +0100

    Ajoutez des fichiers projet.

---
+++ Classes/NelsonSiegelModel.cs
@@ -0,0 +1,31 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_NSS.Classes
+{
+    internal class NelsonSiegelModel
+    {
+        public static double[] CalculerTauxPredits(Vector<double> parametres, double[] maturities)
+        {
+            double beta0 = parametres[0];
+            double beta1 = parametres[1];
+            double beta2 = parametres[2];
+            double beta3 = parametres[3];
+            ;
+
+            double[] tauxPredits = new double[maturities.Length];
+
+            for (int i = 0; i < maturities.Length; i++)
+            {
+                double T = maturities[i];
+                tauxPredits[i] = beta0 + beta1 * (1 - Math.Exp(-T / beta2)) / (T / beta2) + beta3 * ((1 - Math.Exp(-T / beta2)) / (T / beta2) - Math.Exp(-T / beta2)) ;
+            }
+
+            return tauxPredits;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MathNet.Numerics.Optimization;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System.Net;
+
+using Test_NSS.Classes;
+using Extreme.Mathematics.Optimization;
+using MathNet.Numerics;
+using MathNet.Numerics.Statistics;
+using Meta.Numerics.Statistics;
+
+namespace Test_NSS
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] maturites = new double[] { 1,2,3,4,5};
+            double[] tauxObserves= new double[] { 0.02, 0.025, 0.03, 0.035, 0.038 };
+            Vector<double> parametresInitiux =Vector<double>.Build.DenseOfArray(new double[] { 0.01, -0.02, 0.03, 0.02 });
+            Func<Vector<double>, double> fonctionOptimisation = (parametres) =>
+            {
+                double[] taupredicts = NelsonSiegelModel.CalculerTauxPredits(parametres, maturites);
+                double error = 0;
+                for (int i=0;i<maturites.Length;i++)
+                { 
+                    double ecart = tauxObserves[i] - taupredicts[i];
+                    error+= ecart*ecart;
+                }
+                return error;
+            };
+            IObjectiveFunction obj = ObjectiveFunction.Value(fonctionOptimisation);
+            MinimizationResult minresult = NelderMeadSimplex.Minimum(objectiveFunction: obj, initialGuess: parametresInitiux,convergenceTolerance:1e-5,maximumIterations:1000);
+            var FitCs = minresult.MinimizingPoint;//fitted coeffecient
+            var ErrorValue = minresult.FunctionInfoAtMinimum.Value;
+           // Console.WriteLine(minresult.MinimizingPoint.ToString());
+            //var bj = ObjectiveFunction.Value(fonctionOptimisation);
+            //var solver = new NelderMeadSimplex(1e-5, maximumIterations: 100000);
+            // var initialguess = new DenseVector(new[] { 0.01, -0.02, 0.03, 0.02 });
+            //var result =solver.FindMinimum(bj,parametresInitiux);
+            //var resultoptimization = result.MinimizingPoint.ToString();
+
+
+            Console.WriteLine(FitCs);
+            Console.WriteLine(ErrorValue.ToString());
+
+        }
+    }
+}
